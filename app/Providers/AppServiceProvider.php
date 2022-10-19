2c38065491b4ae5482307248fc99cfc146468289
Isaaceldon <awignanisaaceldon@gmail.com>
2022-10-19 01:28:03 +0100

    configuration of the databse infos from the server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,11 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Enable Laravel Migrations Generator for development only
-        if ($this->app->environment() !== 'production') {
-            $this->app->register(\Way\Generators\GeneratorsServiceProvider::class);
-            $this->app->register(\Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
-        }
     }
 
     /**
